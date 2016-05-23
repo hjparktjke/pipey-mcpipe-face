49e5db52f03a53974bac3d71b39aa75a81fc49ae
Michael <hjpark@ucsd.edu>
2016-05-23 14:59:24 -0700

    ADD EVERYTHING

---
+++ clean.sh
@@ -0,0 +1,57 @@
+rm -f assembler/src/*.class
+
+rm -f -r core/src/tb/core_tb/work
+rm -f core/src/tb/core_tb/transcript
+rm -f core/src/tb/core_tb/vsim.wlf
+rm -f core/src/tb/core_tb/*.hex
+rm -f core/src/tb/core_tb/tester_info.txt
+rm -f core/src/tb/core_tb/*trace.txt
+rm -f core/src/tb/core_tb/*trace_ts.txt
+
+rm -f -r core/prj/db
+rm -f -r core/prj/incremental_db
+rm -f -r core/prj/output_files
+
+rm -f -r core/src/tb/miner_tb/work
+rm -f core/src/tb/miner_tb/transcript
+rm -f core/src/tb/miner_tb/vsim.wlf
+rm -f core/src/tb/miner_tb/*.hex
+rm -f core/src/tb/miner_tb/miner_info.txt
+rm -f core/src/tb/miner_tb/*trace.txt
+rm -f core/src/tb/miner_tb/*trace_ts.txt
+
+rm -f -r core/prj/db
+rm -f -r core/prj/incremental_db
+rm -f -r core/prj/output_files
+
+rm -f -r lab1/adder/src/tb/work
+rm -f lab1/adder/src/tb/transcript
+rm -f lab1/adder/src/tb/vsim.wlf
+
+rm -f -r lab1/adder/prj/db
+rm -f -r lab1/adder/prj/incremental_db
+rm -f -r lab1/adder/prj/output_files
+
+rm -f -r lab1/fifo/src/tb/work
+rm -f lab1/fifo/src/tb/transcript
+rm -f lab1/fifo/src/tb/vsim.wlf
+
+rm -f -r lab1/fifo/prj/db
+rm -f -r lab1/fifo/prj/incremental_db
+rm -f -r lab1/fifo/prj/output_files
+
+rm -f -r lab1/reg_file/src/tb/work
+rm -f lab1/reg_file/src/tb/transcript
+rm -f lab1/reg_file/src/tb/vsim.wlf
+
+rm -f -r lab1/reg_file/prj/db
+rm -f -r lab1/reg_file/prj/incremental_db
+rm -f -r lab1/reg_file/prj/output_files
+
+rm -f -r lab2/simple_core/src/tb/work
+rm -f lab2/simple_core/src/tb/transcript
+rm -f lab2/simple_core/src/tb/vsim.wlf
+
+rm -f -r lab2/simple_core/prj/db
+rm -f -r lab2/simple_core/prj/incremental_db
+rm -f -r lab2/simple_core/prj/output_files
